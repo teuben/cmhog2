7d1a75824496c12c209450592ff0fc8852c579f7
Peter Teuben <teuben@astro.umd.edu>
2003-07-18 20:01:21 +0000

    output igrid also

--- grid.c
+++ grid.c
@@ -31,7 +31,7 @@ nemo_main()
   real  ymin = getdparam("ymin");
   real  ymax = getdparam("ymax");
   real yrat, dymin, tol, yr, dfndyr, fn, deltyr, erryr, dz, nzy;
-  int i, iter;
+  int i, iter, igrid;
   int ny = getiparam("ny");
   int nz = getiparam("nz");
   stream outf;
@@ -40,6 +40,7 @@ nemo_main()
   if (hasvalue("dymin")) {   /* Compute yrat from given value of dymin.  
 			      * Newton Raphson iteration
 			      */
+    igrid = 2;
     dymin = getdparam("dymin");
     tol = getdparam("tol");
     yr = 1.01;
@@ -57,6 +58,7 @@ nemo_main()
     yrat = yr;
     dy[0] = dymin;
   } else if (hasvalue("yrat")) {       /*  Compute dy(imin) from given value of yrat. */
+    igrid = 1;
     
     yrat = getdparam("yrat");
     if (yrat == 1.0)
@@ -81,6 +83,6 @@ nemo_main()
   }
   nzy = PI * ymin / dy[0];
   yr = pow(ymax/ymin, 1.0/ny);
-  printf("ny=%d ymin=%g ymax=%g yrat=%g dymin=%g dz=%g nzy=%g bad_yrat=%g\n",
-	 ny,ymin,ymax,yrat,dy[0],dz,nzy,yr);
+  printf("ny=%d ymin=%g ymax=%g yrat=%g dymin=%g dz=%g nzy=%g est_yrat=%g igrid=%d\n",
+	 ny,ymin,ymax,yrat,dy[0],dz,nzy,yr,igrid);
 }
